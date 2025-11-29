46a3d722f4a76c5bc9d3e0d2c7ae2eec63f0ff9c
Bourumir Wyngs <bourumir.wyngs@gmail.com>
2025-11-29 19:47:50 +0100

    Allowing tests that would work

--- tests/extra_bracket.rs
+++ tests/extra_bracket.rs
@@ -1,8 +1,9 @@
+use serde_saphyr::Error;
+
 // saphyr-parser 0.0.6 does not emit closing event.
-#[ignore]
 #[test]
-fn debug_extra_bracket_should_err() {
+fn extra_bracket_should_err() {
     let y = "---\n[ a, b, c ] ]\n";
-    let result: Result<Vec<String>, _> = serde_saphyr::from_str(y);
-    assert!(result.is_err(), "expected error, got Ok: {:?}", result.ok());
+    let result: Error = serde_saphyr::from_str::<serde_json::Value>(y).expect_err("Expected error");
+    assert!(result.to_string().contains("bracket"));
 }
