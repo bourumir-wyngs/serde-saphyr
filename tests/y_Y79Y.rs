44c5ddccd27fe10c9e87ee8613fa5326b7da655d
Bourumir Wyngs <bourumir.wyngs@gmail.com>
2025-11-29 19:46:44 +0100

    Fixed y_Y79Y.rs, tabs cannot be used to indent block scalars, the library is right.

--- tests/y_Y79Y.rs
+++ tests/y_Y79Y.rs
@@ -1,7 +1,5 @@
 use serde::Deserialize;
-
-// Y79Y: Tabs in various contexts
-// The expected mapping is { foo: "\t\n", bar: 1 }.
+use serde_saphyr::Error;
 
 #[derive(Debug, Deserialize)]
 struct Doc {
@@ -9,18 +7,11 @@ struct Doc {
     bar: i64,
 }
 
-// Known issue of serde-saphyr: while standard YAML only disallows tabs in indentation,
-// saphyr rejects any tabs in unquoted scalars. This actually looks quite a sound
-// idea so I am not even sure if it is worth pushing change to saphyr-parser.
 #[test]
-#[ignore]
 fn yaml_y79y_block_scalar_with_tab() {
-    // Use escape sequences for the TAB and newlines to avoid embedding a literal TAB in the file.
-    // Note: Our parser currently errors when the first content line of a block scalar is a TAB.
     let y = "foo: |\n\t\nbar: 1\n";
-    let v: Doc = serde_saphyr::from_str(y).expect("failed to parse Y79Y (tabs in block scalar)");
-    assert_eq!(v.foo, "\t\n");
-    assert_eq!(v.bar, 1);
+    let r: Result<Doc, Error> = serde_saphyr::from_str(y);
+    assert!(r.is_err(), "Tabs cannot be used to indent block scalars");
 }
 
 #[test]
