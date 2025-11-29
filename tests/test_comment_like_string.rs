ba5b24a633e3bec29a6412c31e4b82f1e5b12615
Bourumir Wyngs <bourumir.wyngs@gmail.com>
2025-11-29 12:28:28 +0100

    Comment-like string test

---
+++ tests/test_comment_like_string.rs
@@ -0,0 +1,21 @@
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug, PartialEq)]
+struct CommentLikes {
+    s1: Option<String>,
+    s2: Option<String>,
+}
+
+#[test]
+fn test_comment_like_string() -> anyhow::Result<()> {
+    let yaml = r##"
+        s1: #a
+        s2: "#a"
+    "##;
+
+    let r: CommentLikes = serde_saphyr::from_str(&yaml)?;
+    println!("{:?}", r);
+    assert_eq!(r.s1, None);
+    assert_eq!(r.s2, Some("#a".to_string()));
+    Ok(())
+}
