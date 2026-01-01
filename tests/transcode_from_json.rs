17543e65620c114bee56594b5c5a85239b2da68d
Bourumir Wyngs <bourumir.wyngs@gmail.com>
2026-01-01 12:49:44 +0100

    Transcode is developmental dep only, and now more tests

--- tests/transcode_from_json.rs
+++ tests/transcode_from_json.rs
@@ -2,6 +2,12 @@
 mod tests {
     use serde_saphyr::SerializerOptions;
 
+    fn assert_json_eq(actual_json: &str, expected_json: &str) {
+        let actual: serde_json::Value = serde_json::from_str(actual_json).unwrap();
+        let expected: serde_json::Value = serde_json::from_str(expected_json).unwrap();
+        assert_eq!(actual, expected);
+    }
+
     #[test]
     fn from_sample_platter_json_to_yaml_without_options() {
         let json = r#"
@@ -99,6 +105,88 @@ bools:
       - Language
     self-describing: true
 "#
+        );
+    }
+
+    #[test]
+    fn from_sample_platter_yaml_to_json() {
+        let yaml = r#"rainbow:
+  - red
+  - orange
+  - yellow
+  - green
+  - blue
+  - purple
+point:
+  x: 12
+  y: -34
+bools:
+  - true
+  - false
+"#;
+
+        let v: serde_json::Value = serde_saphyr::from_str(yaml).unwrap();
+        let json = serde_json::to_string(&v).unwrap();
+
+        assert_json_eq(
+            &json,
+            r#"{
+              "rainbow": ["red", "orange", "yellow", "green", "blue", "purple"],
+              "point": {"x": 12, "y": -34},
+              "bools": [true, false]
+            }"#,
+        );
+    }
+
+    #[test]
+    fn from_nested_yaml_to_json() {
+        let yaml = r#"formats:
+  - name: CBOR
+    deacronymization:
+      - Concise
+      - Binary
+      - Object
+      - Representation
+    self-describing: false
+  - name: JSON
+    deacronymization:
+      - JavaScript
+      - Object
+      - Notation
+    self-describing: true
+  - name: YAML
+    deacronymization:
+      - YAML
+      - Ain't
+      - Markup
+      - Language
+    self-describing: true
+"#;
+
+        let v: serde_json::Value = serde_saphyr::from_str(yaml).unwrap();
+        let json = serde_json::to_string(&v).unwrap();
+
+        assert_json_eq(
+            &json,
+            r#"{
+              "formats": [
+                {
+                  "name": "CBOR",
+                  "deacronymization": ["Concise", "Binary", "Object", "Representation"],
+                  "self-describing": false
+                },
+                {
+                  "name": "JSON",
+                  "deacronymization": ["JavaScript", "Object", "Notation"],
+                  "self-describing": true
+                },
+                {
+                  "name": "YAML",
+                  "deacronymization": ["YAML", "Ain't", "Markup", "Language"],
+                  "self-describing": true
+                }
+              ]
+            }"#,
         );
     }
 }
