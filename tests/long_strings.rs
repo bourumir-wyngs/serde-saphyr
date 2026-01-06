add1d2da4755c5e281c141acb66debc867913e48
Bourumir Wyngs <bourumir.wyngs@gmail.com>
2026-01-06 21:35:25 +0100

    Long string test.

--- tests/long_strings.rs
+++ tests/long_strings.rs
@@ -58,3 +58,19 @@ fn yaml_long_strings_ls() {
     let test: Foo = yaml::from_str(serialized.as_str()).expect("Unable to serialize my struct!");
     assert_eq!(reference.long.0, test.long);
 }
+
+#[ignore]
+#[test]
+fn yaml_long_strings_2() {
+    let reference = Foo {
+        a: 32,
+        b: true,
+        short: "A".repeat(20),
+        long: "A".repeat(200)
+    };
+    let serialized = yaml::to_string(&reference).expect("Unable to serialize my struct!");
+    println!("Serialized YAML: r#\"\n{serialized}\"#");
+
+    let test: Foo = yaml::from_str(serialized.as_str()).expect("Unable to serialize my struct!");
+    assert_eq!(reference.long,test.long);
+}
