e0bf1ef37512024ac461045e255034d0bcd41e6c
Bourumir Wyngs <bourumir.wyngs@gmail.com>
2026-01-11 00:03:01 +0100

    Fixed "no panic" test that mades the same assertions.

--- tests/serde_yaml/test_no_panic.rs
+++ tests/serde_yaml/test_no_panic.rs
@@ -138,7 +138,7 @@ fn test_multiline_array() {
     "#;
 
     let parsed: Result<Data, Error> = serde_saphyr::from_str(yaml_input);
-    assert!(parsed.is_err(), "Multiline array should yield an error.");
+    assert!(parsed.is_ok(), "Multiline array should be now ok.");
 
     let correct_yaml_input = r#"
         multiline_array: [
