46e26e4ef7114d70266427178758ab607916e63b
Bourumir Wyngs <bourumir.wyngs@gmail.com>
2026-01-31 19:14:24 +0100

    Disable miri for tests that run the compiler.

--- tests/compile_indent_step.rs
+++ tests/compile_indent_step.rs
@@ -1,6 +1,8 @@
 // `trybuild` runs by spawning a host `cargo` process to compile UI test crates.
 // This is not supported on WASI runtimes, so we disable these tests on WASI.
+// Miri cannot run tests that require spawning processes, so we disable these tests under Miri.
 #![cfg(not(target_os = "wasi"))]
+#![cfg(not(miri))]
 
 #[test]
 fn serializer_options_indent_step_range_is_enforced_for_literals() {
