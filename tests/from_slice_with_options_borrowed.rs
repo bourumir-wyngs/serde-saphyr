53cd2c307e6a30406b840367fad376cb62aeaae0
audrius <audrius.meskauskas@ethz.ch>
2026-02-06 10:09:55 +0100

    New test

---
+++ tests/from_slice_with_options_borrowed.rs
@@ -0,0 +1,17 @@
+use serde::Deserialize;
+
+#[test]
+fn from_slice_with_options_supports_borrowed_str() {
+    #[derive(Debug, Deserialize, PartialEq, Eq)]
+    struct Cfg<'a> {
+        name: &'a str,
+    }
+
+    let yaml = "name: hello\n";
+    let bytes = yaml.as_bytes();
+
+    let cfg: Cfg<'_> = serde_saphyr::from_slice_with_options(bytes, serde_saphyr::Options::default())
+        .expect("deserialize borrowed config");
+
+    assert_eq!(cfg.name, "hello");
+}
