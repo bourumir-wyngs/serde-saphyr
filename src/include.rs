6687cc2a4dfe87f2190d49711540a4c478037ca8
audrius <audrius.meskauskas@ethz.ch>
2026-03-05 09:29:00 +0100

    Adding feature "include"

--- src/include.rs
+++ src/include.rs
@@ -1,5 +1,6 @@
 use saphyr_parser::{BorrowedInput, Parser, StrInput};
 
+#[cfg(feature = "include")]
 #[inline]
 pub(crate) fn create_parser<'a, I>(input: I) -> Parser<'a, I>
 where
@@ -8,6 +9,22 @@ where
     Parser::new(input)
 }
 
+#[cfg(not(feature = "include"))]
+#[inline]
+pub(crate) fn create_parser<'a, I>(input: I) -> Parser<'a, I>
+where
+    I: BorrowedInput<'a>,
+{
+    Parser::new(input)
+}
+
+#[cfg(feature = "include")]
+#[inline]
+pub(crate) fn create_parser_from_str(input: &'_ str) -> Parser<'_, StrInput<'_>> {
+    Parser::new_from_str(input)
+}
+
+#[cfg(not(feature = "include"))]
 #[inline]
 pub(crate) fn create_parser_from_str(input: &'_ str) -> Parser<'_, StrInput<'_>> {
     Parser::new_from_str(input)
