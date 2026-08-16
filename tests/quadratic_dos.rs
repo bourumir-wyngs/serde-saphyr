ff248b587d65b6a7d87d7d98b830efa752ca40f3
Bourumir Wyngs <bourumir.wyngs@gmail.com>
2026-08-16 11:14:44 +0200

    Maybe better to keep the test closer to original

--- tests/quadratic_dos.rs
+++ tests/quadratic_dos.rs
@@ -1,16 +1,8 @@
-use serde_saphyr::{budget, options};
-
 // See https://github.com/acatton/serde-yaml-ng/issues/16 -
 // DoS by parsing deeply nested YAML
 #[test]
 fn test_deep_recursion() {
-    let unlimited = options! {
-        budget: budget! {
-            flow_nesting_limit: 100,
-            max_depth: 1000
-        }
-    };
-    const N: usize = 800;
+    const N: usize = 8000;
     let y = r#"{"":["#.repeat(N) + &r"]}".repeat(N);
-    assert!(serde_saphyr::from_str_with_options::<serde::de::IgnoredAny>(&y, unlimited).is_err());
+    assert!(serde_saphyr::from_str::<serde::de::IgnoredAny>(&y, ).is_err());
 }
